42962701065d1e04f84412f7beffb72baef033ae
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-09-13 20:28:34 -0400

    DONE

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+
+    </div>
+  )
+}
+
+export default Navbar
