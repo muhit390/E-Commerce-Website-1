6fc7ed7779db91d2343a942361e37ceeeefb3197
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-09-14 21:44:22 -0400

    DONE

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,22 +14,22 @@ const Navbar = () => {
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to='/' className='flex flex-col items-center gap-1'>
                 <p>HOME</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
 
             <NavLink to='/collection' className='flex flex-col items-center gap-1'>
                 <p>COLLECTION</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
 
             <NavLink to='/about' className='flex flex-col items-center gap-1'>
                 <p>ABOUT</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
 
             <NavLink to='/contact' className='flex flex-col items-center gap-1'>
                 <p>CONTACT</p>
-                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
 
 
