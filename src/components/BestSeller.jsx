8c99fe01ffa0cea0458d0ddf97fbacd07306741f
Md Jawad Hossain Bhuiyan <muhit.bhuiyan@gmail.com>
2024-09-19 02:49:02 -0400

    DONE

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -7,11 +7,11 @@ import ProductItem from './ProductItem';
 
 const BestSeller = () => {
 
-    const{ products } = useContext(ShopContext);
+    const{products} = useContext(ShopContext);
     const [bestSeller , setBestSeller] = useState([]);
 
     useEffect(()=>{
-        const bestProduct = products.filter((item)=>(item.bestSeller));
+        const bestProduct = products.filter((item)=>(item.bestseller));
         setBestSeller(bestProduct.slice(0,5))
     },[])
   return (
